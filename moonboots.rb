912c874220e2454d49d44a4a2daae71841710abe
Matthew Klein <mtt.l.kln@gmail.com>
2022-01-11 16:16:55 +0000

    cleanup, sub first '/' in href

--- moonboots.rb
+++ moonboots.rb
@@ -22,55 +22,45 @@ files_found.each do |path|
     gemtext_content_arr.each do |line, index|
         # first we handle preformatted text
         if preformatted_text_is_open && line[0..2] != "```"
-            total_html_output << line
-            total_html_output << "\n"
+            total_html_output << line + "\n"
         elsif line[0..2] === "```"
             case preformatted_text_is_open
             when false
-                total_html_output << "<pre>"
-                total_html_output << "\n"
+                total_html_output << "<pre> \n"
                 preformatted_text_is_open = true
             else
-                total_html_output << "</pre>"
-                total_html_output << "\n"
+                total_html_output << "</pre> \n"
                 preformatted_text_is_open = false
             end
         # handle heading #
         elsif line[0..2].include? "# "
-            total_html_output << "<h1>#{line[2..-1]}</h1>"
-            total_html_output << "\n"
+            total_html_output << "<h1>#{line[2..-1]}</h1> \n"
         # handle subheading ##
         elsif line[0..2].include? "## "
-            total_html_output << "<h2>#{line[2..-1]}</h2>"
-            total_html_output << "\n"
+            total_html_output << "<h2>#{line[2..-1]}</h2> \n"
         # handle sub-subheading ###
         elsif line[0..2].include? "###"
-            total_html_output << "<h3>#{line[3..-1]}</h3>"
-            total_html_output << "\n"
+            total_html_output << "<h3>#{line[3..-1]}</h3> \n"
         # handle links
         # TODO conditional if link href ends in image file extension
         elsif line[0..2].include? "=>"
-            line = line.gsub(".gmi", ".html").gsub("'/", '')
+            line = line.gsub(".gmi", ".html").sub("/", "")
             link_href = line.split(" ")[1]
             link_title = line.split(" ")[2..-1].join(" ")
-            total_html_output << "<a href='#{link_href}'>#{link_title}<a/>"
-            total_html_output << "\n"
+            total_html_output << "<a href='#{link_href}'>#{link_title}<a/> <br>"
         # handle list item
         elsif line[0] === "*"
-            total_html_output << "<li>#{line[1..-1]}</li>"
-            total_html_output << "\n"
+            total_html_output << "<li>#{line[1..-1]}</li> \n"
         # handle blockquote
         elsif line[0] === ">"
-            total_html_output << "<blockquote>#{line}</blockquote"
-            total_html_output << "\n"
+            total_html_output << "<blockquote>#{line}</blockquote \n"
         # handle p text
         elsif line.length > 0
-            total_html_output << "<p>#{line}</p>"
+            total_html_output << "<p>#{line}</p> \n"
             total_html_output << "\n"
         #handle empty line
         else
-            total_html_output << "<br>"
-            total_html_output << "\n"
+            total_html_output << "<br> \n"
         end
     end
 
@@ -79,13 +69,13 @@ files_found.each do |path|
         file = File.new("#{output_dir}/#{build}/#{current_dir}/#{current_file[0...-4]}.html", "w")
         file.puts("<head>\n</head>\n<body>\n")
         file.puts(total_html_output)
-        file.puts("</body>")
+        file.puts("</body>\n")
         file.close
     else
         file = File.new("#{output_dir}/#{build}/#{current_file[0...-4]}.html", "w")
         file.puts("<head>\n</head>\n<body>\n")
         file.puts(total_html_output)
-        file.puts("</body>")
+        file.puts("</body>\n")
         file.close
     end
     puts "built html page from #{current_file}"
