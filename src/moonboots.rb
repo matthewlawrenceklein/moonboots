e1ae00a5d51d98ad2b8c159f80309394ef2126c4
Matthew Klein <mtt.l.kln@gmail.com>
2022-01-13 23:25:26 +0000

    hook in css file from config

--- src/moonboots.rb
+++ src/moonboots.rb
@@ -4,7 +4,7 @@ require('yaml')
 
 class MoonBoots
     attr_accessor :root_dir, :root_title, :output_dir, :sub_directories,
-                  :css_file_dir, :font_url, :styling, :preformatted_text_is_open,
+                  :css_file_path, :font_url, :styling, :preformatted_text_is_open,
                   :build
     def initialize
         config = YAML.load_file('/home/matthew/Dev/personal/gemini/src/config.yml')
@@ -12,7 +12,7 @@ class MoonBoots
         @root_title = config['root-title']
         @output_dir = config['output-dir']
         @sub_directories = config['sub-directories']
-        @css_file_dir = config['css-file-dir']
+        @css_file_path = config['css-file-dir']
         @font_url = config['font-url']
         @styling = config['styling']
         @preformatted_text_is_open = false
@@ -105,13 +105,13 @@ class MoonBoots
         if current_dir != @root_dir.split("/")[@root_dir.split("/").length() - 2]
             # `mkdir #{output_dir}/#{build}/#{current_dir}`
             file = File.new("#{@output_dir}/#{@build}/#{current_dir}/#{current_file[0...-4]}.html", "w")
-            file.puts("<head>\n<link rel='stylesheet' href='/../global.css'>\n#{@font_url}\n<title>#{current_dir}</title>\n</head>\n<body>\n<div id='content'>\n")
+            file.puts("<head>\n<link rel='stylesheet' href='/../#{@css_file_path}'>\n#{@font_url}\n<title>#{current_dir}</title>\n</head>\n<body>\n<div id='content'>\n")
             file.puts(total_html_output)
             file.puts("</div>\n</body>\n")
             file.close
         else
             file = File.new("#{output_dir}/#{build}/#{current_file[0...-4]}.html", "w")
-            file.puts("<head>\n<link rel='stylesheet' href='/global.css'>\n#{@font_url}\n<title>#{@root_title}</title>\n</head>\n<body>\n<div id='content'>\n")
+            file.puts("<head>\n<link rel='stylesheet' href='#{@css_file_path}'>\n#{@font_url}\n<title>#{@root_title}</title>\n</head>\n<body>\n<div id='content'>\n")
             file.puts(total_html_output)
             file.puts("</div>\n</body>\n")
             file.close
